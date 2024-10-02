31c2e8c186e36322454dd91129502ca5cc818894
JuliB-dotcom <69867011+JuliaB-dotcom@users.noreply.github.com>
2024-10-02 20:49:41 +0200

    update

---
+++ ToDo/src/main/java/com/todo/demo/models/Task.java
@@ -0,0 +1,11 @@
+package com.todo.demo.models;
+
+import jakarta.persistence.*;
+/*import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;*/
+// Models folder helps define the format of the data at the backend. 
+//Contains all attributes table connection to the database and schema related configurations.
+public class Task {
+  
+}
